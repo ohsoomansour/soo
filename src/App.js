9c0dcb057aeb50c82228f1c49ed7ba53e47a4c75
ohsoomansour <ceoosm@naver.com>
2022-03-15 22:57:41 +0900

    app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./routes/Home"; 
+import Detail from "./routes/Detail";
+/*이 '새로운 App 컴포넌트'는 URL을 보고 있음*/
+/*방법1. npm i react-router-dom@5.3.0(아래) > 방법2. Switch 컴포넌트 > Routes컴포넌트로 대체 
+  https://reactrouter.com/docs/en/v6/getting-started/overview */ 
+/*<Route path="/movie/:id">에서 React Router한테 ':id' id값이 뭔지 알고 싶다는 뜻   */
+function App() {
+  return (
+  <Router>
+    <Switch>
+      <Route path="/hello">
+        <h1>Hello</h1>
+      </Route>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router> 
+  );
+}  
+  export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,31 @@
+/*Movie component가 이 정보들을 parent component로 부터 받아옴*/ 
+/*React Router사용: npm i react-router-dom@5.3.0 > src:route,components 폴더 생성
+  > components로 Movie.js이동 > routes-Home.js생성, Detail.js생성 */
+/*'http://localhost:3000/' Home component 보여줌>'http://localhost:3000/movies/123' Detail component를 보여줌 */   
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+function Movie({id, coverImg, title, summary, genres }) {
+  /* a href="/movie"문제점: '페이지 전체'가 다시 실행(새로고침) > 해결: Link to="/movie"  */
+  return (  
+  <div >
+    <img src={coverImg} alt={title}/>
+    <h2>
+      <Link to={`/movie/${id}`}>{title}</Link>
+    </h2>
+    <p>{summary}</p>
+    <ul>
+      {genres.map((g) => (
+      <li key={g}>{g}</li>
+      ))}
+    </ul>
+  </div>
+  );
+}
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default Movie; 
